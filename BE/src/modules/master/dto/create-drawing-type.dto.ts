9f9760cf3d45f4572e1155c6e34f7af47ed437ed
yutisio12 <yutisiogamings@gmail.com>
2026-06-04 20:03:34 +0700

    BE:
    - create company dto
    - create discipline dto
    - create drawing type dto

--- BE/src/modules/master/dto/create-drawing-type.dto.ts
+++ BE/src/modules/master/dto/create-drawing-type.dto.ts
@@ -1,11 +1,14 @@
 import { IsString, MinLength, MaxLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateDrawingTypeDto {
+  @ApiProperty()
   @IsString()
   @MinLength(2)
   @MaxLength(100)
   name: string;
 
+  @ApiProperty()
   @IsString()
   @MinLength(2)
   @MaxLength(50)
