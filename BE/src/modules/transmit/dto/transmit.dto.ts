41bcedeb6d1483b95210a61d035f268c6bb65207
yutisio12 <yutisiogamings@gmail.com>
2026-06-09 20:25:32 +0700

    BE:
    - trasmit moddule

--- BE/src/modules/transmit/dto/transmit.dto.ts
+++ BE/src/modules/transmit/dto/transmit.dto.ts
@@ -1,11 +1,14 @@
 import { IsArray, IsString, IsOptional, ArrayMinSize } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class TransmitDto {
+  @ApiProperty({ isArray: true })
   @IsArray()
   @ArrayMinSize(1)
   @IsString({ each: true })
   drawing_ids: string[];
 
+  @ApiPropertyOptional()
   @IsOptional()
   @IsString()
   notes?: string;
--- BE/src/modules/transmit/transmit.controller.ts
+++ BE/src/modules/transmit/transmit.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { TransmitService } from './transmit.service';
 import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard';
 import { CurrentUser } from '../../common/decorators/current-user.decorator';
@@ -6,15 +7,19 @@ import { User } from '../../entities/user.entity';
 import { TransmitDto } from './dto/transmit.dto';
 
 @UseGuards(JwtAuthGuard)
+@ApiBearerAuth('JWT')
+@ApiTags('Transmit')
 @Controller('engineering/api')
 export class TransmitController {
   constructor(private transmitService: TransmitService) {}
 
+  @ApiOperation({ summary: 'Get drawings available for transmittal' })
   @Get('drawings/transmittable')
   getTransmittable() {
     return this.transmitService.getTransmittableDrawings();
   }
 
+  @ApiOperation({ summary: 'Transmit drawings' })
   @Post('transmit')
   transmit(@Body() dto: TransmitDto, @CurrentUser() user: User) {
     return this.transmitService.transmit(dto, user.id);
