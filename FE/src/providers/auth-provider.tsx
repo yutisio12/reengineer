afcf05f30c1d340539dad9462bf6db7fb70081d3
Yutisio <44998331+yutisio12@users.noreply.github.com>
2026-08-25 19:49:35 +0700

    Change desc

--- FE/src/providers/auth-provider.tsx
+++ FE/src/providers/auth-provider.tsx
@@ -101,6 +101,6 @@ export function AuthProvider({ children }: { children: ReactNode }) {
 
 export function useAuth() {
   const ctx = useContext(AuthContext)
-  if (!ctx) throw new Error("useAuth must be used within AuthProvider")
+  if (!ctx) throw new Error("useAuth must be used within AuthProvider!")
   return ctx
 }
